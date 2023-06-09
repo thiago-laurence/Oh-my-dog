407b015231dae4bb5eb235538e390aeb170fe8b1
lau-ml <lautaromoller345@gmail.com>
2023-06-09 02:16:18 -0300

    .

---
+++ Oh my dog!/Aplicacion/Controllers/TurnosController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Aplicacion.Models;
+
+namespace Aplicacion.Controllers
+{
+    public class TurnosController : Controller
+    {
+        private readonly OhmydogdbContext _context;
+
+        public TurnosController(OhmydogdbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Turnos
+        public async Task<IActionResult> Index()
+        {
+            var ohmydogdbContext = _context.Turnos.Include(t => t.EstadoNavigation).Include(t => t.PerroNavigation);
+            return View(await ohmydogdbContext.ToListAsync());
+        }
+
+        // GET: Turnos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Turnos == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos
+                .Include(t => t.EstadoNavigation)
+                .Include(t => t.PerroNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            return View(turno);
+        }
+
+        // GET: Turnos/Create
+        public IActionResult Create()
+        {
+            ViewData["Estado"] = new SelectList(_context.EstadoTurnos, "Id", "Id");
+            ViewData["Perro"] = new SelectList(_context.Perros, "Id", "Id");
+            return View();
+        }
+
+        // POST: Turnos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Motivo,Estado,Fecha,Dueno,Perro")] Turno turno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(turno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Estado"] = new SelectList(_context.EstadoTurnos, "Id", "Id", turno.Estado);
+            ViewData["Perro"] = new SelectList(_context.Perros, "Id", "Id", turno.Perro);
+            return View(turno);
+        }
+
+        // GET: Turnos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Turnos == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos.FindAsync(id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+            ViewData["Estado"] = new SelectList(_context.EstadoTurnos, "Id", "Id", turno.Estado);
+            ViewData["Perro"] = new SelectList(_context.Perros, "Id", "Id", turno.Perro);
+            return View(turno);
+        }
+
+        // POST: Turnos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Motivo,Estado,Fecha,Dueno,Perro")] Turno turno)
+        {
+            if (id != turno.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(turno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TurnoExists(turno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Estado"] = new SelectList(_context.EstadoTurnos, "Id", "Id", turno.Estado);
+            ViewData["Perro"] = new SelectList(_context.Perros, "Id", "Id", turno.Perro);
+            return View(turno);
+        }
+
+        // GET: Turnos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Turnos == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos
+                .Include(t => t.EstadoNavigation)
+                .Include(t => t.PerroNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            return View(turno);
+        }
+
+        // POST: Turnos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Turnos == null)
+            {
+                return Problem("Entity set 'OhmydogdbContext.Turnos'  is null.");
+            }
+            var turno = await _context.Turnos.FindAsync(id);
+            if (turno != null)
+            {
+                _context.Turnos.Remove(turno);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TurnoExists(int id)
+        {
+          return (_context.Turnos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
